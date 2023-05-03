0345dc43f5cef361ea954a5657d41d4d6d56ec5b
dev-abulhassan <abulhassan.dev@gmail.com>
2023-05-04 05:52:10 +0600

    API data modify, chef recipe data find with id

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 app.use(cors())
 
 const ourChef = require('./data/ourchef.json');
+// const chef = require('./data/ourchef.json');
 
 app.get('/', (req, res) => {
   res.send('Cookiteer Food Recipe Server')
@@ -13,6 +14,16 @@ app.get('/', (req, res) => {
 
 app.get('/our-chef', (req,res)=>{
    res.send(ourChef)
+})
+
+// app.get('/chef-details', (req, res)=>{
+//    res.send(chef)
+// })
+
+app.get('/our-chef/:id', (req, res)=>{
+   const id =  req.params.id
+   const selectedChef = ourChef.find(item=>item.id == id);
+   res.send(selectedChef)
 })
 
 
